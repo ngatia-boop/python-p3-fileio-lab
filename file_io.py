be6609dd42e322e3a6df164d47b80819d4e9fe0a
ann ngatia <annngatia94@gmail.com>
2025-08-13 20:55:11 +0300

    debug

--- file_io.py
+++ file_io.py
@@ -1,19 +1,16 @@
 def write_file(filename, content):
-    """Writes content to a file, overwriting existing content"""
-    with open(filename, 'w') as f:
+    with open(filename, 'w') as f: #"""Writes content to a file, overwriting existing content"""
         f.write(content)
 
 def append_file(filename, content):
-    """Appends content to a file"""
-    with open(filename, 'a') as f:
+  with open(filename, 'a') as f: # """Appends content to a file"""
         f.write(content + '\n')
 
 def read_file(filename):
-    """Reads and returns file contents"""
-    with open(filename, 'r') as f:
+   with open(filename, 'r') as f: #"""Reads and returns file contents"""
         return f.read()
 
 # Test the file operations
-write_file("logfile.txt", "Log 1: bananas added")
-append_file("logfile.txt", "Log 2: bananas subtracted")
-print(read_file("logfile.txt"))
+#write_file("logfile.txt", "Log 1: bananas added")
+#append_file("logfile.txt", "Log 2: bananas subtracted")
+#print(read_file("logfile.txt"))
